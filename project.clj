f3709a47120fbbe7ced43b5fccc5351626949780
Josh Lehman <jalehman37@gmail.com>
2017-02-24 11:37:41 -0800

    next dev cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity-db "1.3.0"
+(defproject starcity-db "1.4.0-SNAPSHOT"
   :description "The Starcity database schema and migration API."
   :url "https://github.com/starcity-properties/starcity-db"
   :license {:name "Eclipse Public License"
@@ -7,8 +7,7 @@
                  [com.datomic/datomic-free "0.9.5544"]
                  [io.rkn/conformity "0.4.0"]
                  [starcity/toolbelt "0.1.2" :exclusions [com.datomic/datomic-free]]
-                 [datomic-schema "1.3.0"]
-                 [dire "0.5.4"]]
+                 [datomic-schema "1.3.0"]]
 
   :plugins [[s3-wagon-private "1.2.0"]]
 
