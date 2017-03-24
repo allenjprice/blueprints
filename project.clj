07922bd487713523aaa462af9a1f70dcdbe68add
Josh Lehman <jalehman37@gmail.com>
2017-03-24 14:41:59 -0700

    fix broken attr name--don't put `.` in attrs

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity-db "1.4.2-SNAPSHOT"
+(defproject starcity-db "1.4.2"
   :description "The Starcity database schema and migration API."
   :url "https://github.com/starcity-properties/starcity-db"
   :license {:name "Eclipse Public License"
--- src/starcity_db/schema/referral.clj
+++ src/starcity_db/schema/referral.clj
@@ -3,27 +3,21 @@
             [datomic.api :as d]))
 
 (def ^{:added "1.4.1"} schema
-  (conj
-   (s/generate-schema
-    [(s/schema
-      referral
-      (s/fields
-       [source :string :fulltext
-        "The referral source, i.e. the way in which this referral came to us."]
+  (s/generate-schema
+   [(s/schema
+     referral
+     (s/fields
+      [source :string :fulltext
+       "The referral source, i.e. the way in which this referral came to us."]
 
-       [account :ref :index
-        "The account associated with this referral."]
+      [account :ref :idnex
+       "The account associated with this referral."]
 
-       [from :ref :index
-        "The place that this referral came from within our product(s) (enum)."]))])
-   ;; NOTE: Can't use datomic-schema here because of the ident
-   {:db/id                 #db/id[:db.part/db]
-    :db/ident              :referral.tour/community
-    :db/valueType          :db.type/ref
-    :db/cardinality        :db.cardinality/one
-    :db/index              true
-    :db/doc                "The community that this tour referral was booked for."
-    :db.install/_attribute :db.part/db}))
+      [from :ref :index
+       "The place that this referral came from within our product(s) (enum)."]
+
+      [tour-for :ref :index
+       "The community that this tour referral was booked for."]))]))
 
 (defn ^{:added "1.4.1"} referral-from [part]
   [{:db/id    (d/tempid part)
