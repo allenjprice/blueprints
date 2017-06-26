d9cd080d76a299b27b1ed16a957698baa513dacd
Josh Lehman <jalehman37@gmail.com>
2017-06-26 08:44:56 -0700

    add order statuses

--- src/blueprints/schema/order.clj
+++ src/blueprints/schema/order.clj
@@ -3,6 +3,7 @@
   (:require [datomic-schema.schema :as s]
             [datomic.api :as d]))
 
+
 (def ^{:added "1.5.0"} schema
   (s/generate-schema
    [(s/schema
@@ -32,6 +33,30 @@
       [ordered :instant :indexed
        "The instant in time at which this order was placed."]))]))
 
+
+(def ^{:added "1.8.0"} add-order-status
+  (s/generate-schema
+   [(s/schema
+     order
+     (s/fields
+      [status :ref :indexed
+       "The status of this order."]))]))
+
+
+(defn- ^{:added "1.8.0"} add-order-statuses [part]
+  [{:db/id    (d/tempid part)
+    :db/ident :order.status/placed}
+   {:db/id    (d/tempid part)
+    :db/ident :order.status/received}
+   {:db/id    (d/tempid part)
+    :db/ident :order.status/canceled}
+   {:db/id    (d/tempid part)
+    :db/ident :order.status/charged}])
+
+
 (defn norms [part]
   {:schema.order/add-schema-04132017
-   {:txes [schema]}})
+   {:txes [schema]}
+
+   :schema.order/add-statuses-06262017
+   {:txes [add-order-status (add-order-statuses part)]}})
