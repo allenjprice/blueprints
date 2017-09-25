59a97042c765245488146382df5df3335c65abf5
Josh Lehman <jalehman37@gmail.com>
2017-09-25 12:54:27 -0700

    1.11.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/blueprints "1.11.2"
+(defproject starcity/blueprints "1.11.3"
   :description "The Starcity database schema and migration API."
   :url "https://github.com/starcity-properties/blueprints"
   :license {:name "Eclipse Public License"
