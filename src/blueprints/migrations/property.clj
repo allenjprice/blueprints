de3cec84a5314ed8094ae42467d4f56d8c2c523e
Josh Lehman <jalehman37@gmail.com>
2017-11-27 15:16:40 -0800

    add ops fee for orders, add property to payments

--- src/blueprints/migrations/property.clj
+++ src/blueprints/migrations/property.clj
@@ -1,5 +1,12 @@
 (ns blueprints.migrations.property
-  (:require [datomic.api :as d]))
+  (:require [datomic.api :as d]
+            [toolbelt.core :as tb]))
+
+
+;; =============================================================================
+;; add ops fees
+;; =============================================================================
+
 
 (defn- ^{:added "1.2.0"} add-ops-fees
   "Add the Stripe account identifiers of the managed accounts we've created in
@@ -10,6 +17,7 @@
    {:db/id            [:property/internal-name "2072mission"]
     :property/ops-fee 30.0}])
 
+
 (defn- units-by-property-name [conn internal-name]
   (d/q '[:find [?e ...]
          :in $ ?p
@@ -17,15 +25,23 @@
          [?p :property/units ?e]]
        (d/db conn) [:property/internal-name internal-name]))
 
+
 (defn- name-units [conn internal-name]
   (->> (units-by-property-name conn internal-name)
        (map-indexed
         (fn [i e]
           [:db/add e :unit/name (format "%s-%s" internal-name (inc i))]))))
 
+
 (defn- ^{:added "1.2.0"} add-unit-names [conn]
   (concat (name-units conn "52gilbert")
           (name-units conn "2072mission")))
+
+
+;; =============================================================================
+;; add tours
+;; =============================================================================
+
 
 (def ^{:added "1.4.1"} add-tours
   "For our new Schedule Tour page to work, each property needs to have a value
@@ -37,6 +53,38 @@
    {:db/id          [:property/internal-name "2072mission"]
     :property/tours true}])
 
+
+;; =============================================================================
+;; add payment properties
+;; =============================================================================
+
+
+(defn ^{:added "1.17.0"} add-payment-properties [conn]
+  (->> (d/q '[:find ?py ?pr
+              :where
+              [?py :payment/account ?a]
+              [?a :account/licenses ?l]
+              [?l :member-license/unit ?u]
+              [?pr :property/units ?u]]
+            (d/db conn))
+       (tb/distinct-by first)
+       (mapv
+        (fn [[py pr]]
+          [:db/add py :payment/property pr]))))
+
+
+;; =============================================================================
+;; add orders ops fees
+;; =============================================================================
+
+
+(def ^{:added "1.17.0"} add-orders-ops-fees
+  [{:db/id                   [:property/internal-name "52gilbert"]
+    :property/ops-fee-orders 100.0}
+   {:db/id                   [:property/internal-name "2072mission"]
+    :property/ops-fee-orders 0.0}])
+
+
 (defn norms [conn]
   {:migration.property/add-ops-fees-12-15-16
    {:txes [(add-ops-fees conn)]}
@@ -45,4 +93,10 @@
    {:txes [(add-unit-names conn)]}
 
    :migration.property/add-tours-03232017
-   {:txes [add-tours]}})
+   {:txes [add-tours]}
+
+   :migration.property/add-payment-property-11262017
+   {:txes [(add-payment-properties conn)]}
+
+   :migration.property/add-orders-ops-fees
+   {:txes [add-orders-ops-fees]}})
