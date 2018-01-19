8b6b93a94819203b1e398c94fce775920392d3f4
Josh Lehman <jalehman37@gmail.com>
2018-01-19 13:44:19 -0800

    improve selectors, add specs

--- src/blueprints/models/application.clj
+++ src/blueprints/models/application.clj
@@ -27,37 +27,148 @@
 ;; =============================================================================
 
 
-(def account (comp first :account/_member-application))
+(defn account
+  "The account that owns this application."
+  [application]
+  (-> application :account/_member-application first))
 
-(def desired-license :application/license)
-
-(def move-in-date :application/move-in)
-
-(def communities :application/communities)
-
-(def community-fitness :application/fitness)
-
-(def address :application/address)
-
-(def has-pet? :application/has-pet)
-
-(def pet :application/pet)
-
-(def completed-at :application/submitted-at)
-
-(def status :application/status)
+(s/fdef account
+        :args (s/cat :application td/entityd?)
+        :ret td/entityd?)
 
 
-(defn last-modified-at
-  [db application]
-  (let [app-inst (td/updated-at db application)
-        cf-inst  (td/updated-at db (community-fitness application))
-        app-ms   (inst-ms app-inst)
-        cf-ms    (inst-ms cf-inst)]
-    (get
-     {app-ms app-inst
-      cf-ms  cf-inst}
-    (max app-ms cf-ms))))
+(defn desired-license
+  "The desired license term."
+  [application]
+  (:application/license application))
+
+(s/fdef desired-license
+        :args (s/cat :application td/entity?)
+        :ret (s/nilable td/entityd?))
+
+
+(defn move-in-date
+  "The desired move in date."
+  [application]
+  (:application/move-in application))
+
+(s/fdef move-in-date
+        :args (s/cat :application td/entity?)
+        :ret (s/nilable inst?))
+
+
+(defn communities
+  "The desired communities that this applicant would like to live in."
+  [application]
+  (:application/communities application))
+
+(s/fdef communities
+        :args (s/cat :application td/entity?)
+        :ret (s/* td/entityd?))
+
+
+(defn community-fitness
+  "The community fitness questionnaire."
+  [application]
+  (:application/fitness application))
+
+(s/fdef community-fitness
+        :args (s/cat :application td/entity?)
+        :ret (s/nilable td/entityd?))
+
+
+(defn address
+  "The current address of this applicant."
+  [application]
+  (:application/address application))
+
+(s/fdef address
+        :args (s/cat :application td/entity?)
+        :ret (s/nilable td/entityd?))
+
+
+(defn has-pet?
+  "Does this applicant have a pet?"
+  [application]
+  (:application/has-pet application))
+
+(s/fdef has-pet?
+        :args (s/cat :application td/entity?)
+        :ret (s/nilable boolean?))
+
+
+(defn pet
+  "Information about this applicant's pet."
+  [application]
+  (:application/pet application))
+
+(s/fdef pet
+        :args (s/cat :application td/entity?)
+        :ret (s/nilable td/entityd?))
+
+
+(defn completed-at
+  [application]
+  (:application/submitted-at application))
+
+(s/fdef completed-at
+        :args (s/cat :application td/entity?)
+        :ret (s/nilable inst?))
+
+
+(defn status
+  "The status of this application."
+  [application]
+  (:application/status application))
+
+(s/fdef status
+        :args (s/cat :application td/entity?)
+        :ret (s/nilable ::status))
+
+
+(def ^:private community-fitness-labels
+  {:fitness/skills
+   "How will you contribute to the community?"
+   :fitness/free-time
+   "What do you like to do in your free time?"
+   :fitness/dealbreakers
+   "Do you have any dealbreakers?"
+   :fitness/experience
+   "Describe your past experience(s) living in shared spaces."
+   :fitness/interested
+   "Please tell the members why you want to join their community."
+   :fitness/conflicts
+   "Please describe how you would resolve a conflict between yourself and another member of the home."})
+
+
+(s/def :fitness/label
+  string?)
+
+(s/def :fitness/key
+  #{:fitness/skills :fitness/free-time :fitness/dealbreakers
+    :fitness/experience :fitness/interested :fitness/conflicts})
+
+(s/def :fitness/value
+  (s/nilable string?))
+
+
+(defn community-fitness-labeled
+  "Produce a vector of community fitness answersw with their associated
+  questions."
+  [application]
+  (let [cf (community-fitness application)]
+    (reduce
+     (fn [acc k]
+       (conj acc {:label (get community-fitness-labels k)
+                  :key   k
+                  :value (get cf k)}))
+     []
+     [:fitness/experience :fitness/skills :fitness/free-time
+      :fitness/dealbreakers :fitness/interested :fitness/conflicts])))
+
+(s/fdef community-fitness-labeled
+        :args (s/cat :application td/entity?)
+        :ret (s/+ (s/keys :req-un [:fitness/label :fitness/key :fitness/value])))
 
 
 ;; =============================================================================
@@ -153,7 +264,33 @@
 
 (s/fdef by-account
         :args (s/cat :db td/db? :account td/entity?)
-        :ret (s/or :nothing nil? :entity td/entity?))
+        :ret (s/nilable td/entityd?))
+
+
+(defn last-modified-at
+  "The date that this application was last modified at."
+  [db application]
+  (let [app-inst (td/updated-at db application)
+        cf-inst  (when-let [cf (community-fitness application)]
+                   (td/updated-at db cf))
+        app-ms   (inst-ms app-inst)
+        cf-ms    (when (some? cf-inst) (inst-ms cf-inst))]
+    (cond
+      (= app-ms cf-ms) app-inst
+
+      (nil? cf-ms) app-inst
+
+      :otherwise
+      (get
+       {app-ms app-inst
+        cf-ms  cf-inst}
+       (if (nil? cf-ms)
+         app-ms
+         (max app-ms cf-ms))))))
+
+(s/fdef last-modified-at
+        :args (s/cat :db td/db? :application td/entity?)
+        :ret inst?)
 
 
 ;; =============================================================================
