f3b9c69ded30863cd049a9c16ff6cfba6ad0587d
Josh Lehman <jalehman37@gmail.com>
2018-01-19 13:43:47 -0800

    add proper selectors & specs

--- src/blueprints/models/address.clj
+++ src/blueprints/models/address.clj
@@ -1,4 +1,6 @@
-(ns blueprints.models.address)
+(ns blueprints.models.address
+  (:require [clojure.spec.alpha :as s]
+            [toolbelt.datomic :as td]))
 
 
 ;; =============================================================================
@@ -6,13 +8,46 @@
 ;; =============================================================================
 
 
-(def locality :address/locality)
-(def region :address/region)
-(def postal-code :address/postal-code)
+(defn country
+  [address]
+  (:address/country address))
+
+(s/fdef country
+        :args (s/cat :address td/entity?)
+        :ret (s/nilable string?))
 
 
-;; =====================================
-;; Convenience
+(defn locality
+  "Locality, i.e. municipality/city."
+  [address]
+  (:address/locality address))
+
+(s/fdef locality
+        :args (s/cat :address td/entity?)
+        :ret (s/nilable string?))
+
+
+(defn region
+  "Region, i.e. state/province."
+  [address]
+  (:address/region address))
+
+(s/fdef region
+        :args (s/cat :address td/entity?)
+        :ret (s/nilable string?))
+
+
+(defn postal-code
+  "Postal code, i.e. zip code."
+  [address]
+  (:address/postal-code address))
+
+(s/fdef postal-code
+        :args (s/cat :address td/entity?)
+        :ret (s/nilable string?))
+
+
+;; convenience ==========================
 
 (def city locality)
 (def state region)
