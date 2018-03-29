48db01d2757f82df528a9cae5f0cc01620b9b2f5
Allen J Price <allen@starcity.com>
2018-03-29 14:29:57 -0700

    add random helper functions to orders seed
    for filling in service fields randomly

--- src/blueprints/seed/orders.clj
+++ src/blueprints/seed/orders.clj
@@ -1,11 +1,40 @@
 (ns blueprints.seed.orders
   (:require [blueprints.models.order :as order]
             [blueprints.seed.utils :as utils]
-            [datomic.api :as d]))
+            [datomic.api :as d]
+            [clojure.string :as string]))
 
+
+(defn rand-int-to-5
+  "random number between 1 and 5 "
+  []
+  (inc (rand-int 5)))
+
+
+(defn rand-string
+  "random lorem ipsum sentences"
+  []
+  (-> "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.  Donec hendrerit tempor tellus.  Donec pretium posuere tellus.  Proin quam nisl, tincidunt et, mattis eget, convallis nec, purus.  Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.  Nulla posuere.  Donec vitae dolor.  Nullam tristique diam non turpis.  Cras placerat accumsan nulla.  Nullam rutrum.  Nam vestibulum accumsan nisl. "
+      (string/split #".  ")
+      (rand-nth)))
+
+
+(defn rand-inst
+  "random instance between today and three weeks from today"
+  []
+  (utils/days-from-now (inc (rand-int 21))))
+
+
+;; random option from a dropdown
+(defn rand-option
+  "randomly choose an option from a list of options"
+  [options]
+  (rand-nth options))
+
+;; fill in a service's fields randomly
 
 (defn order
-  [account-id service-id & {:keys [quantity desc variant status price]
+  [account-id service-id & {:keys [quantity desc variant status price cost fields]
                             :or   {status :order.status/pending}}]
   (toolbelt.core/assoc-when
    {:db/id         (utils/tempid)
@@ -14,8 +43,10 @@
     :order/account account-id
     :order/status  status}
    :order/price (when-let [p price] (float p))
+   :order/cost (when-let [c cost] (float c))
    :order/variant variant
    :order/quantity (when-let [q quantity] (float q))
+   :order/fields fields
    :order/desc desc))
 
 
