edf5a0dd2ee66388d444bff02edbb7ae69b93507
therese <tdiede@gmail.com>
2018-04-12 14:39:57 -0700

    update id >> entity

--- src/blueprints/models/service.clj
+++ src/blueprints/models/service.clj
@@ -171,14 +171,14 @@
   (:service/active service false))
 
 
-(defn ^{:added "3.0.0"} plan-id
-  "The teller plan id of this `service`, if any."
+(defn ^{:added "3.0.0"} plan
+  "The teller plan of this `service`, if any."
   [service]
-  (:service/plan-id service))
+  (:service/plan service))
 
-(s/fdef service-id
+(s/fdef plan
         :args (s/cat :service td/entity?)
-        :ret (s/nilable uuid?))
+        :ret (s/nilable td/entityd?))
 
 
 ;; =============================================================================
