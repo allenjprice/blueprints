47dde4a750855dd846576cb44fb9fd71cf912476
Josh Lehman <jalehman37@gmail.com>
2018-04-24 11:25:53 -0700

    use partition directly

--- src/blueprints/models/service.clj
+++ src/blueprints/models/service.clj
@@ -375,7 +375,7 @@
                            type          :service.type/service
                            archived      false}}]
    (tb/assoc-some
-    {:db/id                 (tds/tempid)
+    {:db/id                 :db.part/starcity
      :service/code          code
      :service/name          name
      :service/desc          desc
