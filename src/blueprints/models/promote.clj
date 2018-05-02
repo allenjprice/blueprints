30f2710854cdbb0cf8bc9ab116ee7628f4c54895
Josh Lehman <jalehman37@gmail.com>
2018-05-02 15:31:53 -0700

    don't create payment here anymore

--- src/blueprints/models/promote.clj
+++ src/blueprints/models/promote.clj
@@ -3,34 +3,13 @@
             [blueprints.models.approval :as approval]
             [blueprints.models.member-license :as member-license]
             [blueprints.models.property :as property]
-            [blueprints.models.payment :as payment]
             [blueprints.models.security-deposit :as deposit]
             [blueprints.models.unit :as unit]
             [clj-time.coerce :as c]
             [clj-time.core :as t]
             [clojure.spec.alpha :as s]
-            [toolbelt.core :as tb]
             [toolbelt.date :as date]
             [toolbelt.datomic :as td]))
-
-
-(defn- prorated-amount [commencement rate]
-  (let [commencement   (c/to-date-time commencement)
-        days-in-month  (t/day (t/last-day-of-the-month commencement))
-        ;; We inc the days-remaining so that the move-in day is included in the calculation
-        days-remaining (inc (- days-in-month (t/day commencement)))]
-    (tb/round (* (/ rate days-in-month) days-remaining) 2)))
-
-
-(defn- prorated-payment [property account start rate]
-  (let [tz    (property/time-zone property)
-        start (date/beginning-of-day start tz)]
-    (payment/create (prorated-amount start rate) account
-                    :for :payment.for/rent
-                    :pstart start
-                    :pend (date/end-of-month start tz)
-                    :status :payment.status/due
-                    :due start)))
 
 
 (defn- security-deposit-due-date
@@ -63,15 +42,8 @@
           "`account` must be approved before it can be promoted!")
   (let [base           (account/change-role account :account.role/member)
         approval       (approval/by-account account)
-        member-license (approval->member-license approval)
-        payment        (prorated-payment (approval/property approval)
-                                         account
-                                         (approval/move-in approval)
-                                         (member-license/rate member-license))]
-    [(->> (tb/assoc-when
-           member-license
-           :member-license/rent-payments payment)
-          (assoc base :account/license))
+        member-license (approval->member-license approval)]
+    [(assoc base :account/license member-license)
      (security-deposit-due-date account approval)]))
 
 (s/fdef promote
