68a51a059da4526f01b56fd324e13597e0561aed
Allen J Price <allenjprice@gmail.com>
2018-06-27 15:45:44 -0700

    Version 2.7.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starcity/blueprints "2.6.3-SNAPSHOT"
+(defproject starcity/blueprints "2.7.0-SNAPSHOT"
   :description "The Starcity database schema and migration API."
   :url "https://github.com/starcity-properties/blueprints"
   :license {:name "Eclipse Public License"
